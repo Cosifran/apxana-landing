5a668a5944df9a4324be268ad97b417e9eb2f492
Francisco Estrada <franciscobackend@gmail.com>
2025-10-14 17:20:06 -0500

    (FEATURE) create structure of components

---
+++ app/components/we.tsx
@@ -0,0 +1,7 @@
+export default function We() {
+  return (
+    <div>
+      We
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+//Import components
+import Nav from "./components/nav";
+import Footer from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container mx-auto">
+          <Nav />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
