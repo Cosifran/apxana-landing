60afcc65852290af9f2ea3e732c7da1874a44159
Francisco Estrada <franciscobackend@gmail.com>
2025-10-15 09:34:53 -0500

    (FEATURE)  Creating the left side of the hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,7 +2,7 @@
 import DotGrid from "@/components/DotGrid";
 export default function Hero() {
   return (
-    <div className="h-screen">
+    <div className="h-screen relative">
       <DotGrid
         dotSize={4}
         gap={15}
@@ -18,6 +18,26 @@ export default function Hero() {
           height: "100%",
         }}
       />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="grid grid-cols-2 gap-4 container">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-4xl font-bold">Impulsamos tu negocio con tecnologia</h1>
+            <p>
+              Somos una firma de consultoría IT que diseña, implementa y
+              optimiza soluciones modernas para acelerar resultados y crear
+              ventajas competitivas.
+            </p>
+            <div className="flex items-center gap-2">
+              <button className="bg-primary text-white px-4 py-2 rounded-md">
+                Agenda una demo
+              </button>
+              <button className="bg-gray-400 text-primary px-4 py-2 rounded-md">
+                Conoce mas
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
