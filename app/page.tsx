b4407cf04f3de49e91a7c5b599c6988cf99dd8b4
AndresOsM <andres.mejia522@outlook.es>
2025-10-17 15:06:59 -0500

    Seccion de servicios con iconos y diseño responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 // Import components
 import Hero from "./components/hero";
 import Contact from "./components/contact";
+import Services from "./components/services";
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
       <Contact />
     </>
   );
