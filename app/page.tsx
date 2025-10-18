fd1722c836b95ed1791ff17a02bbe2267098bbb6
AndresOsM <andres.mejia522@outlook.es>
2025-10-17 21:54:31 -0500

    Seccion porque elegirnos, y se agregan logos para mostrar nuestra tecnologias

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import Hero from "./components/hero";
 import Contact from "./components/contact";
 import Services from "./components/services";
 import We from "./components/we";
+import WhyChooseUs from "./components/why-choose-us";
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
       <We />
+      <WhyChooseUs />
       <Contact />
     </>
   );
