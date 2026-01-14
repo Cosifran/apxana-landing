51e4dec34b38b7aa3a70b01e79df7ab2e9bea4d7
Francisco Estrada <franciscobackend@gmail.com>
2026-01-14 09:09:13 -0500

    feat: The Motion library is added and tested in hero.

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,19 +1,23 @@
-'use client';
+"use client";
 // Import components
 import Image from "next/image";
 import DotGrid from "@/components/DotGrid";
+// Import motion
+import { motion } from "motion/react";
 
 export default function Hero() {
-
   const scrollToSection = (id: string) => {
     const section = document.querySelector(id);
-    if(section) {
+    if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   return (
-    <div id="inicio" className="relative h-[978px] lg:h-screen flex items-center justify-center overflow-hidden bg-gradient-to-b from-[#d4cce8] to-[#ffffff] pt-24 md:pt-0">
+    <div
+      id="inicio"
+      className="relative h-[978px] lg:h-screen flex items-center justify-center overflow-hidden bg-gradient-to-b from-[#d4cce8] to-[#ffffff] pt-24 md:pt-0"
+    >
       <DotGrid
         dotSize={4}
         gap={15}
@@ -36,32 +40,39 @@ export default function Hero() {
               Impulsamos tu negocio con tecnologia
             </h1>
             <p className="text-gray-700 text-base sm:text-lg ">
-              Somos una empresa dedicada a ayudar a otras empresas y emprendedores a convertir sus ideas en 
-              soluciones digitales. Ofrecemos consultoría tecnológica, desarrollo de software a la medida 
-              y diseño digital con un enfoque innovador y escalable.
+              Somos una empresa dedicada a ayudar a otras empresas y
+              emprendedores a convertir sus ideas en soluciones digitales.
+              Ofrecemos consultoría tecnológica, desarrollo de software a la
+              medida y diseño digital con un enfoque innovador y escalable.
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
-              <button className="bg-primary text-white px-6 py-3 rounded-md font-medium hover:opacity-90 transition" 
-                onClick={() => scrollToSection('#contacto')}>
+              <button
+                className="bg-primary text-white px-6 py-3 rounded-md font-medium hover:opacity-90 transition"
+                onClick={() => scrollToSection("#contacto")}
+              >
                 Contactanos
               </button>
-              <button className="bg-gray-200 text-primary px-6 py-3 rounded-md font-medium hover:bg-gray-300 transition"
-                onClick={() => scrollToSection('#nosotros')}>
+              <button
+                className="bg-gray-200 text-primary px-6 py-3 rounded-md font-medium hover:bg-gray-300 transition"
+                onClick={() => scrollToSection("#nosotros")}
+              >
                 Conoce mas
               </button>
             </div>
           </div>
 
           <div className="flex justify-center md:justify-end mt-10 md:mt-0">
-            <div className="relative rounded-2xl overflow-hidden shadow-2xl transform hover:scale-[1.02] transition duration-500">
-              <Image
-                src="/assets/page-hero.png"
-                alt="Dashboard tecnológico Apxana"
-                width={800}
-                height={500}
-                className="rounded-xl object-contain md:object-cover w-[85vw] sm:w-[500px] md:w-[600px] h-auto"
-                priority
-              />
+            <div className="relative rounded-2xl overflow-hidden transform hover:scale-[1.02] transition duration-500">
+              <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
+                <Image
+                  src="/assets/page-hero.png"
+                  alt="Dashboard tecnológico Apxana"
+                  width={800}
+                  height={500}
+                  className="rounded-xl object-contain md:object-cover w-[85vw] sm:w-[500px] md:w-[600px] h-auto"
+                  priority
+                />
+              </motion.div>
             </div>
           </div>
         </div>
